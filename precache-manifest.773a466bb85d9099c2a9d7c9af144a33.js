6121cd2a46cdca5cb3022f9dbe845b0a8c7d5d38
zhuyidai <zhuyidai>
2020-01-31 19:39:21 -0500

    Basic Deploy File

---
+++ precache-manifest.773a466bb85d9099c2a9d7c9af144a33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb509a6688c49adec1c450a6d9474012",
+    "url": "/nbareact/index.html"
+  },
+  {
+    "revision": "93bb64546882e16f1f08",
+    "url": "/nbareact/static/js/2.5b99958e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/nbareact/static/js/2.5b99958e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95f2174bcded5eebe68d",
+    "url": "/nbareact/static/js/3.a8a487fa.chunk.js"
+  },
+  {
+    "revision": "725fdb9742952cea97c0",
+    "url": "/nbareact/static/js/main.5a41005a.chunk.js"
+  },
+  {
+    "revision": "4a9f33a4f746c6e835a2",
+    "url": "/nbareact/static/js/runtime-main.124a649a.js"
+  },
+  {
+    "revision": "a71d303bb77b372bc335c0ef8264566b",
+    "url": "/nbareact/static/media/kobe1.a71d303b.jpg"
+  },
+  {
+    "revision": "4c68c8141e07004261dc3c306c8d3723",
+    "url": "/nbareact/static/media/nba1.4c68c814.png"
+  }
+]);
